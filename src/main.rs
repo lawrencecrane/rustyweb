1b2633756c373a0b4cba14242823acc6904d6d48
Lauri Kurki <lauri.kurki@siili.com>
2019-09-21 16:55:57 +0300

    Add set_timeouts and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, BufReader, BufRead, Error, IoSlice};
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
@@ -12,28 +13,37 @@ fn main() {
 fn connect(stream: Result<TcpStream, Error>) {
     match stream {
         Ok(stream) => {
-            request(&stream);
-            respond(&stream);
-        }
-        Err(_) => { /* connection failed */ }
-    }
-}
-
-fn respond(mut stream: &TcpStream) {
-    match stream.write_vectored(&response_ok("Hello from the otherside")) {
-        Ok(_) => true,
-        Err(_) => false
+            get_request(&stream).and_then(|_| respond(&stream))
+                .unwrap_or_default();
+        },
+        Err(_) => {}
     };
 }
 
-fn request(stream: &TcpStream) -> Vec<String> {
+fn respond(mut stream: &TcpStream) -> Result<usize, Error> {
+    stream.set_write_timeout(Some(Duration::new(2, 0)))
+        .unwrap();
+
+    stream.write_vectored(&response_ok("Hello from the otherside"))
+}
+
+fn get_request(stream: &TcpStream) -> Result<Vec<String>, Error> {
+    stream.set_read_timeout(Some(Duration::new(2, 0)))
+        .unwrap();
+
     let reader = BufReader::new(stream);
 
     reader.split('\n' as u8)
-        .map(Result::unwrap)
-        .take_while(|xs| xs.len() > 1)
-        .map(|xs| String::from_utf8(xs).unwrap())
+        .take_while(|x| x.as_ref().unwrap_or(&Vec::new()).len() > 1)
+        .map(parse_request)
         .collect()
+}
+
+fn parse_request(xs: Result<Vec<u8>, Error>) -> Result<String, Error> {
+    match xs {
+        Ok(line) => Ok(String::from_utf8(line).unwrap()),
+        Err(e) => Err(e)
+    }
 }
 
 fn response_ok(msg: &str) -> [IoSlice; 2] {
