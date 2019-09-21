e44945a31f4ecaa71495f7391441d733fe48f81f
Lauri Kurki <lauri.kurki@siili.com>
2019-09-21 15:53:38 +0300

    Read request and use IoSlice in writes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Write, Error};
+use std::io::{Write, BufReader, BufRead, Error, IoSlice};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
@@ -12,23 +12,31 @@ fn main() {
 fn connect(stream: Result<TcpStream, Error>) {
     match stream {
         Ok(stream) => {
-            respond(stream);
+            request(&stream);
+            respond(&stream);
         }
         Err(_) => { /* connection failed */ }
     }
 }
 
-fn respond(mut stream: TcpStream) {
-    match stream.write(&response_ok("Hello from the otherside")) {
-        Ok(_) => {
-            true;
-        }
-        Err(_) => {
-            false;
-        }
-    }
+fn respond(mut stream: &TcpStream) {
+    match stream.write_vectored(&response_ok("Hello from the otherside")) {
+        Ok(_) => true,
+        Err(_) => false
+    };
 }
 
-fn response_ok(msg: &str) -> Vec<u8> {
-    ["HTTP/1.1 200 OK \n\n", msg].concat().as_bytes().to_vec()
+fn request(stream: &TcpStream) -> Vec<String> {
+    let reader = BufReader::new(stream);
+
+    reader.split('\n' as u8)
+        .map(Result::unwrap)
+        .take_while(|xs| xs.len() > 1)
+        .map(|xs| String::from_utf8(xs).unwrap())
+        .collect()
+}
+
+fn response_ok(msg: &str) -> [IoSlice; 2] {
+    [IoSlice::new("HTTP/1.1 200 OK\n\n".as_bytes()),
+     IoSlice::new(msg.as_bytes())]
 }
