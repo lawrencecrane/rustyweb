59ab2cdb1779144c25e1c310ed0df0f0e83b84e5
Lauri Kurki <lauri.kurki@siili.com>
2019-09-22 12:42:45 +0300

    Just move respond function in file

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,18 @@ pub mod http;
 
 type ResponderType = fn(&TcpStream, http::request::Request) -> Result<(), Error>;
 
+pub fn respond(stream: &TcpStream, msg: &str) -> Result<(), Error> {
+    let mut responder = BufWriter::new(stream);
+
+    match responder.write(&http::response::ok(
+        msg,
+        vec!["Content-Type: text/html; charset=utf-8".to_string()]
+    ).to_bytes()) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e)
+    }
+}
+
 pub fn serve(host: &str, port: isize, responder: ResponderType) {
     let listener = TcpListener::bind([host, ":", &port.to_string()].concat()).unwrap();
 
@@ -27,16 +39,4 @@ fn inquire(stream: &TcpStream) -> Result<http::request::Request, Error> {
     let inquirer = BufReader::new(stream);
 
     http::request::parse(inquirer)
-}
-
-pub fn respond(stream: &TcpStream, msg: &str) -> Result<(), Error> {
-    let mut responder = BufWriter::new(stream);
-
-    match responder.write(&http::response::ok(
-        msg,
-        vec!["Content-Type: text/html; charset=utf-8".to_string()]
-    ).to_bytes()) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e)
-    }
 }
