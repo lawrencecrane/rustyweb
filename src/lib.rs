3a63a39c6bfef38a3dbc324dbcd2a0d2bdddf06e
Lauri Kurki <lauri.kurki@siili.com>
2019-09-22 12:40:53 +0300

    Restructure responding phase
    Serve caller must provide responder function to handle reponse writes

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,42 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Write, BufWriter, BufReader, Error, ErrorKind};
+use std::io::{Write, BufWriter, BufReader, Error};
 
-mod http;
+pub mod http;
 
-pub fn serve(host: &str, port: isize) {
+type ResponderType = fn(&TcpStream, http::request::Request) -> Result<(), Error>;
+
+pub fn serve(host: &str, port: isize, responder: ResponderType) {
     let listener = TcpListener::bind([host, ":", &port.to_string()].concat()).unwrap();
 
     for stream in listener.incoming() {
-        connect(stream);
+        connect(stream, responder);
     }
 }
 
-fn connect(stream: Result<TcpStream, Error>) {
+fn connect(stream: Result<TcpStream, Error>, responder: ResponderType) {
     match stream {
         Ok(stream) => {
-            inquire(&stream).and_then(|request| respond(&stream, request))
+            inquire(&stream).and_then(|request| responder(&stream, request))
                 .unwrap_or_default();
         },
         Err(_) => {}
     };
 }
 
-fn respond(stream: &TcpStream, request: http::request::Request) -> Result<usize, Error> {
-    match (&request.request_line().method, request.request_line().uri.as_ref()) {
-        (http::request::Method::GET, "/") => {
-            get_root(stream)
-        },
-        _ => {
-            Err(Error::new(ErrorKind::NotFound, "404"))
-        }
-    }
-}
-
-fn get_root(stream: &TcpStream) -> Result<usize, Error> {
-    let mut responder = BufWriter::new(stream);
-
-    responder.write(&http::response::ok(
-        "Hello from the otherside",
-        vec!["Content-Type: text/html; charset=utf-8".to_string()]
-    ).to_bytes())
-}
-
 fn inquire(stream: &TcpStream) -> Result<http::request::Request, Error> {
     let inquirer = BufReader::new(stream);
 
     http::request::parse(inquirer)
+}
+
+pub fn respond(stream: &TcpStream, msg: &str) -> Result<(), Error> {
+    let mut responder = BufWriter::new(stream);
+
+    match responder.write(&http::response::ok(
+        msg,
+        vec!["Content-Type: text/html; charset=utf-8".to_string()]
+    ).to_bytes()) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::net::TcpStream;
+use std::io::{Error, ErrorKind};
+
 extern crate rustyweb;
 
 fn main() {
-    rustyweb::serve("0.0.0.0", 80)
+    rustyweb::serve("0.0.0.0", 80, respond)
+}
+
+fn respond(stream: &TcpStream,
+           request: rustyweb::http::request::Request) -> Result<(), Error> {
+    match (&request.request_line().method, request.request_line().uri.as_ref()) {
+        (rustyweb::http::request::Method::GET, "/") => {
+            rustyweb::respond(stream, "Hello from the otherside")
+        },
+        _ => {
+            Err(Error::new(ErrorKind::NotFound, "404"))
+        }
+    }
 }
