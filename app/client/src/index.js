c5d829e0a455d5cb900a0a0b9ceb0835eb4dca98
Lauri Kurki <lauri.kurki@siili.com>
2019-09-27 18:00:01 +0300

    Add websocket code to client

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -1,1 +1,9 @@
-console.log('hello from the otherside');
+const ws = new WebSocket('ws://localhost:8080');
+
+ws.addEventListener('open', () => {
+  ws.send('Hello from client side');
+});
+
+ws.addEventListener('message', event => {
+  console.log('Got:', event.data);
+});
